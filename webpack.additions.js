69bc9c6d7f3ac75b82f253017523a1d1a73170dc
Zviad Metreveli <thezviad@gmail.com>
2021-02-05 09:02:49 -0500

    actually use robot font as material ui wants it

--- webpack.additions.js
+++ webpack.additions.js
@@ -1,4 +1,19 @@
 module.exports = (config) => {
+	for (const rule of config.module.rules) {
+		if (".scss".match(rule.test)) {
+			// Need to use `resolve-url-loader` to get
+			// @import "~@fontsource/roboto" to work in `styles.scss`.
+			const use = [
+				'style-loader',
+				'css-loader',
+				'resolve-url-loader',
+				'sass-loader',
+			]
+			console.info(`WEBPACK[custom]: for: ${rule.test}, ${rule.use} -> ${use}`)
+			rule.use = use
+		}
+	}
+
 	// SVG is already configured for `url-loader` so insert @svgr/webpack loader
 	// at the beginning of the rules list to apply after url-loader.
 	config.module.rules.unshift({
