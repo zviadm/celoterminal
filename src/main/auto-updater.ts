73bfaddddd28c09c62252ec5dfe463406fc8b451
Zviad Metreveli <thezviad@gmail.com>
2021-02-04 19:59:35 -0500

    [core/autoupdater] set force quit before relaunch (#12)
    * [core/autoupdater] set force quit before relaunch
    * make force quit work better

--- src/main/auto-updater.ts
+++ src/main/auto-updater.ts
@@ -2,6 +2,8 @@ import { app, ipcMain } from 'electron'
 import { autoUpdater, UpdateInfo } from '@ledgerhq/electron-updater'
 import log from 'electron-log'
 
+import { setForceQuit } from '.'
+
 let _updateReady: UpdateInfo | undefined
 export const setupAutoUpdater = (): void => {
 	if (app.isPackaged) {
@@ -29,8 +31,12 @@ export const setupAutoUpdater = (): void => {
 		event.returnValue = _updateReady?.version
 	})
 	ipcMain.on("quit-and-install", (event) => {
+		log.info(`autoupdater: preparing to exit to install update...`)
+		setForceQuit()
 		app.relaunch()
+		log.info(`autoupdater: relaunch flag set...`)
 		autoUpdater.quitAndInstall()
+		log.info(`autoupdater: quit-and-install called`)
 		event.returnValue = null
 	})
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -8,6 +8,7 @@ app.allowRendererProcessReuse = true
 // Global reference to mainWindow (necessary to prevent window from being garbage collected).
 let mainWindow: BrowserWindow | null
 let willQuitApp = false
+export const setForceQuit = (): void => { willQuitApp = true }
 
 function createMainWindow() {
   const minWidth = 850
@@ -64,7 +65,7 @@ app.on('before-quit', () => willQuitApp = true);
 // Quit application when all windows are closed (except for macOS).
 app.on('window-all-closed', () => {
   // on macOS it is common for applications to stay open until the user explicitly quits.
-  if (process.platform !== 'darwin') {
+  if (willQuitApp || process.platform !== 'darwin') {
     app.quit()
   }
 })
