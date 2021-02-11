3167ff1a0819645a52eb6b516f1e4af53ea3ab40
Zviad Metreveli <thezviad@gmail.com>
2021-02-11 11:18:50 -0500

    [app/governance] add simple governance voting app (#42)

--- src/renderer/apps/apps.ts
+++ src/renderer/apps/apps.ts
@@ -1,5 +1,6 @@
 import { AppDefinition } from '../components/app-definition'
 import { Celovote } from './celovote/def'
+import { Governance } from './governance/def'
 import { Locker } from './locker/def'
 import { Mento } from './mento/def'
 import { SendReceive } from './send-receive/def'
@@ -8,6 +9,7 @@ export const AppList: AppDefinition[] = [
 	// Core Apps.
 	SendReceive,
 	Locker,
+	Governance,
 
 	// Optional Apps.
 	Mento,
