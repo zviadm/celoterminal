77dac5c7485b7b0e461d37ccc29bc5cd604513b8
Zviad Metreveli <thezviad@gmail.com>
2021-02-12 14:36:39 -0500

    nit adjust

--- src/renderer/state/time.ts
+++ src/renderer/state/time.ts
@@ -1,6 +1,7 @@
+let _adjustedMS = 0
+
 // Code should use nowMs instead of Date.now() directly, since nowMs
 // allows test code to adjust time, for timing sensitive test scenarios.
-let _adjustedMS = 0
 export const nowMS = (): number => {
 	return Date.now() + _adjustedMS
 }
