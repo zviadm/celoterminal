4dc5ff614b0c36596df999aca053b147ab0078d5
Zviad Metreveli <thezviad@gmail.com>
2021-03-03 17:17:50 -0500

    [app/governance] use standard AppContainer/AppSection elements

--- src/renderer/apps/governance/governance.tsx
+++ src/renderer/apps/governance/governance.tsx
@@ -13,13 +13,15 @@ import { nowMS } from '../../state/time'
 
 import * as React from 'react'
 import {
-	Box, Button, Paper, Table, TableBody,
+	Box, Button, Table, TableBody,
 	TableCell, TableHead, TableRow
 } from '@material-ui/core'
 import { Alert, AlertTitle } from '@material-ui/lab'
 
 import AppHeader from '../../components/app-header'
 import Link from '../../components/link'
+import AppSection from '../../components/app-section'
+import AppContainer from '../../components/app-container'
 
 const GovernanceApp = (props: {
 	accounts: Account[],
@@ -112,164 +114,148 @@ const GovernanceApp = (props: {
 	}
 
 	return (
-		<Box display="flex" flexDirection="column" flex={1}>
+		<AppContainer>
 			<AppHeader app={Governance} isFetching={isFetching} refetch={refetch} />
 			{fetched && <>
-			<Box marginTop={2}>
-				<Paper>
-					<Box p={2} display="flex" flexDirection="column">
-						<Alert severity="info">
-						Use <Link href="https://celo.stake.id">celo.stake.id</Link> to view more in-depth
-						information about all past and currently active governance proposals.
-						</Alert>
-					</Box>
-				</Paper>
-			</Box>
+			<AppSection>
+				<Alert severity="info">
+				Use <Link href="https://celo.stake.id">celo.stake.id</Link> to view more in-depth
+				information about all past and currently active governance proposals.
+				</Alert>
+			</AppSection>
 			{fetched.proposals.length === 0 && fetched.upvotes.length === 0 &&
-			<Box marginTop={2}>
-				<Paper>
-					<Box p={2} display="flex" flexDirection="column">
-						<Alert severity="info">
-							There are no active governance proposals to upvote or vote right now.
-						</Alert>
-					</Box>
-				</Paper>
-			</Box>}
+			<AppSection>
+				<Alert severity="info">
+					There are no active governance proposals to upvote or vote right now.
+				</Alert>
+			</AppSection>}
 			{fetched.proposals.length > 0 &&
-			<Box marginTop={2}>
-				<Paper>
-					<Box p={2} display="flex" flexDirection="column">
-						{fetched.isAccount ?
-						<Alert severity="info">
-							Voting on a proposal disables unlocking of CELO until
-							proposal is finished.
-						</Alert> :
-						<Alert severity="error">
-							To participate in governance,
-							you need to first register your address and lock CELO using the Locker app.
-						</Alert>}
-						<Table size="small">
-							<TableHead>
-								<TableRow>
-									<TableCell>ID</TableCell>
-									<TableCell width="100%">Votes</TableCell>
-									<TableCell></TableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-							{
-								fetched.proposals.map((p) => {
-									const vote = fetched.voteRecords.find((v) => v.proposalID.eq(p.proposalID))
-									const total = p.votes.Yes.plus(p.votes.No).plus(p.votes.Abstain)
-									return (
-									<TableRow key={p.proposalID.toString()}>
-										<TableCell>
-											<ProposalID id={p.proposalID} />
-										</TableCell>
-										<TableCell padding="none" style={{paddingBottom: 10, paddingTop: 10}}>
-											<Alert severity={p.passing ? "success" : "error"}>
-												<AlertTitle>{p.passing ? "Passing" : "Not Passing"}</AlertTitle>
-												{`Time left: ${p.timeUntilExecution}\u2026`}
-											</Alert>
-											<Table size="small">
-												<TableBody>
-													<TableRow>
-														<TableCell>Yes</TableCell>
-														<TableCell width="100%">{fmtAmount(p.votes.Yes, "CELO", 0)}</TableCell>
-														<TableCell>{p.votes.Yes.div(total).multipliedBy(100).toFixed(0)}%</TableCell>
-													</TableRow>
-													<TableRow>
-														<TableCell>No</TableCell>
-														<TableCell>{fmtAmount(p.votes.No, "CELO", 0)}</TableCell>
-														<TableCell>{p.votes.No.div(total).multipliedBy(100).toFixed(0)}%</TableCell>
-													</TableRow>
-													<TableRow>
-														<TableCell>Abstain</TableCell>
-														<TableCell>{fmtAmount(p.votes.Abstain, "CELO", 0)}</TableCell>
-														<TableCell>{p.votes.Abstain.div(total).multipliedBy(100).toFixed(0)}%</TableCell>
-													</TableRow>
-												</TableBody>
-											</Table>
-										</TableCell>
-										<TableCell>
-											<Box display="flex" flexDirection="column">
-											{
-												([VoteValue.Yes, VoteValue.No, VoteValue.Abstain] as
-												("Yes" | "No" | "Abstain")[]).map((v) => (
-													<Box key={v} marginBottom={0.5}>
-														<Button
-															id={`vote-${v}-${p.proposalID.toString()}`}
-															style={{width: 100}}
-															variant={vote?.value === v ? "contained" : "outlined"}
-															color={vote?.value === v ? "primary" : "default"}
-															disabled={!fetched.isAccount}
-															onClick={() => { handleVote(p.proposalID, v) }}
-															>{v}</Button>
-													</Box>
-												))
-											}
+			<AppSection>
+				{fetched.isAccount ?
+				<Alert severity="info">
+					Voting on a proposal disables unlocking of CELO until
+					proposal is finished.
+				</Alert> :
+				<Alert severity="error">
+					To participate in governance,
+					you need to first register your address and lock CELO using the Locker app.
+				</Alert>}
+				<Table size="small">
+					<TableHead>
+						<TableRow>
+							<TableCell>ID</TableCell>
+							<TableCell width="100%">Votes</TableCell>
+							<TableCell></TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+					{
+						fetched.proposals.map((p) => {
+							const vote = fetched.voteRecords.find((v) => v.proposalID.eq(p.proposalID))
+							const total = p.votes.Yes.plus(p.votes.No).plus(p.votes.Abstain)
+							return (
+							<TableRow key={p.proposalID.toString()}>
+								<TableCell>
+									<ProposalID id={p.proposalID} />
+								</TableCell>
+								<TableCell padding="none" style={{paddingBottom: 10, paddingTop: 10}}>
+									<Alert severity={p.passing ? "success" : "error"}>
+										<AlertTitle>{p.passing ? "Passing" : "Not Passing"}</AlertTitle>
+										{`Time left: ${p.timeUntilExecution}\u2026`}
+									</Alert>
+									<Table size="small">
+										<TableBody>
+											<TableRow>
+												<TableCell>Yes</TableCell>
+												<TableCell width="100%">{fmtAmount(p.votes.Yes, "CELO", 0)}</TableCell>
+												<TableCell>{p.votes.Yes.div(total).multipliedBy(100).toFixed(0)}%</TableCell>
+											</TableRow>
+											<TableRow>
+												<TableCell>No</TableCell>
+												<TableCell>{fmtAmount(p.votes.No, "CELO", 0)}</TableCell>
+												<TableCell>{p.votes.No.div(total).multipliedBy(100).toFixed(0)}%</TableCell>
+											</TableRow>
+											<TableRow>
+												<TableCell>Abstain</TableCell>
+												<TableCell>{fmtAmount(p.votes.Abstain, "CELO", 0)}</TableCell>
+												<TableCell>{p.votes.Abstain.div(total).multipliedBy(100).toFixed(0)}%</TableCell>
+											</TableRow>
+										</TableBody>
+									</Table>
+								</TableCell>
+								<TableCell>
+									<Box display="flex" flexDirection="column">
+									{
+										([VoteValue.Yes, VoteValue.No, VoteValue.Abstain] as
+										("Yes" | "No" | "Abstain")[]).map((v) => (
+											<Box key={v} marginBottom={0.5}>
+												<Button
+													id={`vote-${v}-${p.proposalID.toString()}`}
+													style={{width: 100}}
+													variant={vote?.value === v ? "contained" : "outlined"}
+													color={vote?.value === v ? "primary" : "default"}
+													disabled={!fetched.isAccount}
+													onClick={() => { handleVote(p.proposalID, v) }}
+													>{v}</Button>
 											</Box>
-										</TableCell>
-									</TableRow>)
-								})
-							}
-							</TableBody>
-						</Table>
-					</Box>
-				</Paper>
-			</Box>}
+										))
+									}
+									</Box>
+								</TableCell>
+							</TableRow>)
+						})
+					}
+					</TableBody>
+				</Table>
+			</AppSection>}
 			{fetched.upvotes.length > 0 &&
-			<Box marginTop={2}>
-				<Paper>
-					<Box p={2} display="flex" flexDirection="column">
-						{fetched.isAccount ?
-						<Alert severity="info">
-							Only one proposal can be upvoted at a time.
-						</Alert> :
-						<Alert severity="error">
-							To participate in governance,
-							you need to first register your address and lock CELO using the Locker app.
-						</Alert>}
-						<Table size="small">
-							<TableHead>
-								<TableRow>
-									<TableCell>ID</TableCell>
-									<TableCell width="100%">Upvotes</TableCell>
-									<TableCell></TableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-							{
-								fetched.upvotes.map((v) => {
-									const isUpvoted = v.proposalID.eq(fetched.upvoteRecord.proposalID)
-									return (
-									<TableRow key={v.proposalID.toString()}>
-										<TableCell>
-											<ProposalID id={v.proposalID} />
-										</TableCell>
-										<TableCell>{fmtAmount(v.upvotes, "CELO", 0)}</TableCell>
-										<TableCell>
-											<Box display="flex" flexDirection="column">
-											<Button
-												id={`upvote-${v.proposalID.toString()}`}
-												style={{width: 100}}
-												variant={isUpvoted ? "contained" : "outlined"}
-												color={isUpvoted ? "primary" : "default"}
-												disabled={!fetched.isAccount}
-												onClick={() => { handleUpvote(v.proposalID) }}
-												>{isUpvoted ? "Upvoted" : "Upvote"}</Button>
-											</Box>
-										</TableCell>
-									</TableRow>)
-								})
-							}
-							</TableBody>
-						</Table>
-					</Box>
-				</Paper>
-			</Box>}
+			<AppSection>
+				{fetched.isAccount ?
+				<Alert severity="info">
+					Only one proposal can be upvoted at a time.
+				</Alert> :
+				<Alert severity="error">
+					To participate in governance,
+					you need to first register your address and lock CELO using the Locker app.
+				</Alert>}
+				<Table size="small">
+					<TableHead>
+						<TableRow>
+							<TableCell>ID</TableCell>
+							<TableCell width="100%">Upvotes</TableCell>
+							<TableCell></TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+					{
+						fetched.upvotes.map((v) => {
+							const isUpvoted = v.proposalID.eq(fetched.upvoteRecord.proposalID)
+							return (
+							<TableRow key={v.proposalID.toString()}>
+								<TableCell>
+									<ProposalID id={v.proposalID} />
+								</TableCell>
+								<TableCell>{fmtAmount(v.upvotes, "CELO", 0)}</TableCell>
+								<TableCell>
+									<Box display="flex" flexDirection="column">
+									<Button
+										id={`upvote-${v.proposalID.toString()}`}
+										style={{width: 100}}
+										variant={isUpvoted ? "contained" : "outlined"}
+										color={isUpvoted ? "primary" : "default"}
+										disabled={!fetched.isAccount}
+										onClick={() => { handleUpvote(v.proposalID) }}
+										>{isUpvoted ? "Upvoted" : "Upvote"}</Button>
+									</Box>
+								</TableCell>
+							</TableRow>)
+						})
+					}
+					</TableBody>
+				</Table>
+			</AppSection>}
 			</>}
-		</Box>
+		</AppContainer>
 	)
 }
 export default GovernanceApp
