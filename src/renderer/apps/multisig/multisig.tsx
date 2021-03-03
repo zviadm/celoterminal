05e1ed40a08512869ea0a3f89d188c1d09071839
Zviad Metreveli <thezviad@gmail.com>
2021-03-03 11:34:23 -0500

    tx confirmation layout

--- src/renderer/apps/multisig/multisig.tsx
+++ src/renderer/apps/multisig/multisig.tsx
@@ -1,6 +1,13 @@
+import BigNumber from 'bignumber.js'
+import { ContractKit } from '@celo/contractkit'
+
 import { Account } from '../../../lib/accounts'
 import { TXFunc, TXFinishFunc } from '../../components/app-definition'
 import { MultiSig } from './def'
+import { explorerRootURL } from '../../../lib/cfg'
+import { fmtAddress } from '../../../lib/utils'
+import useLocalStorageState from '../../state/localstorage-state'
+import useOnChainState from '../../state/onchain-state'
 
 import * as React from 'react'
 import {
@@ -8,18 +15,14 @@ import {
 	Typography
 } from '@material-ui/core'
 import { Alert, TabContext, TabList, TabPanel } from '@material-ui/lab'
+import { Add } from '@material-ui/icons'
 
 import AppContainer from '../../components/app-container'
 import AppHeader from '../../components/app-header'
 import AppSection from '../../components/app-section'
-import useOnChainState from '../../state/onchain-state'
-import { ContractKit } from '@celo/contractkit'
 import Link from '../../components/link'
-import { explorerRootURL } from '../../../lib/cfg'
-import { fmtAddress } from '../../../lib/utils'
-import { Add } from '@material-ui/icons'
-import useLocalStorageState from '../../state/localstorage-state'
 import SectionTitle from '../../components/section-title'
+import { contractName } from '../../../lib/registry'
 
 const MultiSigApp = (props: {
 	accounts: Account[],
@@ -37,12 +40,22 @@ const MultiSigApp = (props: {
 				return {}
 			}
 			const multisig = await kit.contracts.getMultiSig(account.address)
-			const transactions = multisig.getTransactions()
+			const transactionsP = multisig.getTransactions()
 			const owners = multisig.getOwners()
 			const requiredSigs = multisig.getRequired()
 			const requiredInternalSigs = multisig.getInternalRequired()
+
+			const transactions = (await transactionsP).map((t, idx) => ({...t, idx: idx}))
+			const pendingTXs = transactions.filter((t) => !t.executed)
+			const destinations = Array.from(new Set(transactions.map((t) => t.destination)))
+			const contractNames = await Promise.all(destinations.map((d) => contractName(kit, d)))
+			const contractNameMap = new Map(
+				destinations.map((d, idx) => [d, contractNames[idx]])
+			)
 			return {
-				transactions: await transactions,
+				transactions,
+				pendingTXs,
+				contractNameMap,
 				owners: await owners,
 				requiredSigs: await requiredSigs,
 				requiredInternalSigs: await requiredInternalSigs,
@@ -51,6 +64,13 @@ const MultiSigApp = (props: {
 		[account]
 	))
 	const [tab, setTab] = useLocalStorageState("terminal/multisig/tab", "transactions")
+
+	const requiredConfirms = (
+		destination: string,
+		requiredSigs: BigNumber,
+		requiredInternalSigs: BigNumber) => {
+		return (destination === account.address) ? requiredInternalSigs : requiredSigs
+	}
 
 	return (
 		<AppContainer>
@@ -69,16 +89,57 @@ const MultiSigApp = (props: {
 						<Tab label="Owners" value={"owners"} />
 					</TabList>
 					<TabPanel value="transactions">
-						{!fetched.transactions?.length ?
+						{!fetched?.pendingTXs?.length ?
 						<Alert severity="info">
 							There are no pending transactions.
 						</Alert> :
 						<Box>
-							Pending transactions: {fetched.transactions?.length}
+							<Table size="small">
+								<TableHead>
+									<TableCell>ID</TableCell>
+									<TableCell width="100%">Contract & Data</TableCell>
+									<TableCell align="right">Confirms</TableCell>
+									<TableCell></TableCell>
+								</TableHead>
+								<TableBody>
+									{
+										fetched.pendingTXs.map((t) => {
+											const required = requiredConfirms(t.destination, fetched.requiredSigs, fetched.requiredInternalSigs)
+											const canExecute = required.lte(t.confirmations.length)
+											const confirmedBySelf = t.confirmations.indexOf(account.ownerAddress) >= 0
+											return (
+												<TableRow key={t.idx}>
+													<TableCell>{t.idx.toString()}</TableCell>
+													<TableCell>{fetched.contractNameMap.get(t.destination)}</TableCell>
+													<TableCell align="right">
+														{t.confirmations.length} / {required.toString()}
+													</TableCell>
+													<TableCell>
+														{canExecute ?
+														<Button
+															variant="outlined"
+															color="primary"
+														>Execute</Button> : (
+															!confirmedBySelf ?
+															<Button
+																variant="outlined"
+																color="primary"
+															>Confirm</Button> :
+															<Button
+																variant="outlined"
+																color="secondary"
+															>Revoke</Button>
+														)}
+													</TableCell>
+												</TableRow>
+											)
+										})
+									}
+								</TableBody>
+							</Table>
 						</Box>}
 					</TabPanel>
 					<TabPanel value="owners">
-						<SectionTitle>Owners</SectionTitle>
 						<Table size="small">
 							<TableBody>
 								{fetched.owners?.map((o) => {
@@ -108,29 +169,31 @@ const MultiSigApp = (props: {
 								Add Owner
 							</Button>
 						</Box>
-						<Box display="flex" flexDirection="column" marginTop={6}>
-							<SectionTitle>Required signatures</SectionTitle>
-							<Table size="small">
-								<TableBody>
-									<TableRow>
-										<TableCell width="100%">For executing transactions</TableCell>
-										<TableCell>{fetched.requiredSigs?.toString()}</TableCell>
-										<TableCell>
-											<Button variant="outlined" color="primary">Change</Button>
-										</TableCell>
-									</TableRow>
-									<TableRow>
-										<TableCell width="100%">For changing MultiSig properties</TableCell>
-										<TableCell>{fetched.requiredInternalSigs?.toString()}</TableCell>
-										<TableCell>
-											<Button variant="outlined" color="primary">Change</Button>
-										</TableCell>
-									</TableRow>
-								</TableBody>
-							</Table>
-						</Box>
 					</TabPanel>
 				</AppSection>
+				{tab === "owners" && <>
+				<AppSection>
+					<SectionTitle>Required signatures</SectionTitle>
+					<Table size="small">
+						<TableBody>
+							<TableRow>
+								<TableCell width="100%">For executing transactions</TableCell>
+								<TableCell>{fetched.requiredSigs?.toString()}</TableCell>
+								<TableCell>
+									<Button variant="outlined" color="secondary">Change</Button>
+								</TableCell>
+							</TableRow>
+							<TableRow>
+								<TableCell width="100%">For changing MultiSig properties</TableCell>
+								<TableCell>{fetched.requiredInternalSigs?.toString()}</TableCell>
+								<TableCell>
+									<Button variant="outlined" color="secondary">Change</Button>
+								</TableCell>
+							</TableRow>
+						</TableBody>
+					</Table>
+				</AppSection>
+				</>}
 			</TabContext>
 			</>}
 		</AppContainer>
