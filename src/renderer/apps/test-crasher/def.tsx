1aa735ca2bbea4dcfc6ba802fa586cb655dcbc03
Zviad Metreveli <thezviad@gmail.com>
2021-03-17 16:53:05 -0400

    merge with main

--- src/renderer/apps/test-crasher/def.tsx
+++ src/renderer/apps/test-crasher/def.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { AppDefinition } from "../../components/app-definition"
 import CrasherApp from "./crasher"
 import BugReport from "@material-ui/icons/BugReport"
@@ -5,7 +6,8 @@ import BugReport from "@material-ui/icons/BugReport"
 export const Crasher: AppDefinition = {
 	id: "test-crasher",
 	title: "Crasher",
-	icon: BugReport,
+	icon: <BugReport />,
+	iconLarge: <BugReport fontSize="large" />,
 	description: `
 	This is a test-only app that crashes right away. Useful to test Celo Terminal
 	to make sure single crashing app doesn't crash the whole app.
