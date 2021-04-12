6d32fba698f0830cae20ca09b2331e88fc034c14
Zviad Metreveli <thezviad@gmail.com>
2021-04-12 13:18:11 -0400

    fix registry test

--- src/lib/erc20/registry.test.ts
+++ src/lib/erc20/registry.test.ts
@@ -4,22 +4,17 @@ import { isValidAddress } from 'ethereumjs-util'
 import { erc20Registry } from "./registry"
 
 test('sanity check registry constants', () => {
-	for (const erc20s of [erc20Registry]) {
-		const symbolSet = new Set<string>()
-		const addrSet = new Set<string>()
-		erc20s.forEach((e) => {
-			Object.values(e.addresses).forEach((addr) => {
-				if (!addr) {
-					return
-				}
-				const normalizedAddr = ensureLeading0x(toChecksumAddress(addr))
-				expect(addr).toEqual(normalizedAddr)
-				expect(isValidAddress(addr)).toEqual(true)
-			})
-			symbolSet.add(e.symbol)
+	const symbolSet = new Set<string>()
+	erc20Registry.forEach((e) => {
+		Object.values(e.addresses).forEach((addr) => {
+			if (!addr) {
+				return
+			}
+			const normalizedAddr = ensureLeading0x(toChecksumAddress(addr))
+			expect(addr).toEqual(normalizedAddr)
+			expect(isValidAddress(addr)).toEqual(true)
 		})
-
-		expect(erc20s.length).toEqual(symbolSet.size)
-		expect(erc20s.length).toEqual(addrSet.size)
-	}
+		symbolSet.add(e.symbol)
+	})
+	expect(erc20Registry.length).toEqual(symbolSet.size)
 })
