150b3c87c34b57da802a68d1cf64e47fbe87fdbc
Zviad Metreveli <thezviad@gmail.com>
2021-05-10 17:59:43 -0400

    [app/celovote] update icon viewbox

--- src/renderer/apps/celovote/def.tsx
+++ src/renderer/apps/celovote/def.tsx
@@ -8,9 +8,8 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 const CVIcon = (props: {fontSize?: "default" | "large"}): JSX.Element => {
 	return <SvgIcon
 		fontSize={props.fontSize}
-		viewBox="0 0 500 400"
+		viewBox="0 0 397 391"
 		component={CelovoteIcon}
-		style={{paddingLeft: 5}}
 		/>
 }
 
