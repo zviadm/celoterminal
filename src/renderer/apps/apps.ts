1ef9baf2bae9b3b6f369bfd232def2ce5eaa3b61
Zviad Metreveli <thezviad@gmail.com>
2021-05-16 22:25:12 -0400

    [app/wc] remove wallet-connect app once again (#184)

--- src/renderer/apps/apps.ts
+++ src/renderer/apps/apps.ts
@@ -9,7 +9,7 @@ import { MultiSig } from './multisig/def'
 import { Portfolio } from './portfolio/def'
 import { SavingsCELO } from './savingscelo/def'
 import { SendReceive } from './send-receive/def'
-import { WalletConnect } from './wallet-connect/def'
+// import { WalletConnect } from './wallet-connect/def'
 import { Crasher } from './test-crasher/def'
 
 export const AppList: AppDefinition[] = [
@@ -18,7 +18,7 @@ export const AppList: AppDefinition[] = [
 	SendReceive,
 	Locker,
 	Governance,
-	WalletConnect,
+	// WalletConnect,
 
 	// Optional Apps.
 	Mento,
