fdea4a3c634ca84bd15d1924edb41e4885faaee0
Zviad Metreveli <thezviad@gmail.com>
2021-10-10 15:10:23 -0400

    [core] fix auto update background check (#225)

--- src/main/auto-updater.ts
+++ src/main/auto-updater.ts
@@ -13,7 +13,7 @@ export const setupAutoUpdater = (): void => {
 		autoUpdater.allowPrerelease = false
 		runWithInterval(
 			"main-update",
-			autoUpdater.checkForUpdates,
+			() => { return autoUpdater.checkForUpdates() },
 			30 * 60 * 1000,
 		) // Check every 30 minutes.
 		autoUpdater.on("error", (e: Error) => {
