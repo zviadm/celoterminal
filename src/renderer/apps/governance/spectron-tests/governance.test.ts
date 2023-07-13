24d4389a20082c04e8fd71638c5f6b1e9164e077
Zviad Metreveli <thezviad@gmail.com>
2023-07-13 07:16:03 -0400

    upvoting is still a thing, bring back tests for it (#299)

--- src/renderer/apps/governance/spectron-tests/governance.test.ts
+++ src/renderer/apps/governance/spectron-tests/governance.test.ts
@@ -11,6 +11,7 @@ test('Governance upvote & vote', async (done) => {
 	const kit = devchainKit()
 	await createGovernanceProposal(kit)
 	await createGovernanceProposal(kit)
+	await createGovernanceProposal(kit)
 
 	const accounts = await kit.contracts.getAccounts()
 	const lockedGold = await kit.contracts.getLockedGold()
@@ -20,6 +21,34 @@ test('Governance upvote & vote', async (done) => {
 	await selectApp("governance")
 
 	const governance = await kit.contracts.getGovernance()
+
+	// Proposal 1 should be automatically dequeued and ready for voting.
+	const voteYes1 = await app.client.$("#vote-Yes-1")
+	await voteYes1.waitForEnabled()
+	await voteYes1.click()
+	await confirmTXs()
+	const votes1 = await governance.getVotes(1)
+	expect(votes1.Yes.toNumber()).toEqual(101e18)
+	const isApproved1 = await governance.isApproved(1)
+	expect(isApproved1).toEqual(false)
+
+	// Switch upvotes between Proposal 2 & 3.
+	const upvote2 = await app.client.$("#upvote-2")
+	await upvote2.isEnabled()
+	await upvote2.click()
+	await confirmTXs()
+	const upvotes2 = await governance.getUpvotes(2)
+	expect(upvotes2.toNumber()).toEqual(101e18)
+	const upvote3 = await app.client.$("#upvote-3")
+	await upvote3.isEnabled()
+	await upvote3.click()
+	await confirmTXs({txCount: 2}) // needs to revoke previous upvote.
+	const upvotes2after = await governance.getUpvotes(2)
+	expect(upvotes2after.toNumber()).toEqual(0)
+	const upvotes3 = await governance.getUpvotes(3)
+	expect(upvotes3.toNumber()).toEqual(101e18)
+
+	// Dequeues both Proposal 2 & 3 by advancing time.
 	const cfg = await governance.getConfig()
 	await adjustNow(cfg.dequeueFrequency.multipliedBy(1000).toNumber())
 	await dequeueAndApproveProposal(kit, 2)
@@ -34,16 +63,6 @@ test('Governance upvote & vote', async (done) => {
 	expect(votes2.Yes.toNumber()).toEqual(101e18)
 	const isApproved2 = await governance.isApproved(2)
 	expect(isApproved2).toEqual(true)
-
-	// Voting no longer needs Approval, make sure can vote on non-approved proposal 1 too.
-	const voteYes1 = await app.client.$("#vote-Yes-1")
-	await voteYes1.waitForEnabled()
-	await voteYes1.click()
-	await confirmTXs()
-	const votes1 = await governance.getVotes(1)
-	expect(votes1.Yes.toNumber()).toEqual(101e18)
-	const isApproved1 = await governance.isApproved(1)
-	expect(isApproved1).toEqual(false)
 
 	const voteNo2 = await app.client.$("#vote-No-2")
 	await voteNo2.click()
