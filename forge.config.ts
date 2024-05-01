4416d7d67f9d4eb957cd5ee72eb097ef6f99c1a7
Zviad Metreveli <thezviad@gmail.com>
2024-05-01 15:37:37 -0400

    it sort of runs

--- forge.config.ts
+++ forge.config.ts
@@ -21,6 +21,7 @@ const config: ForgeConfig = {
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
       mainConfig,
+      devContentSecurityPolicy: "connect-src 'self' * 'unsafe-eval'",
       renderer: {
         config: rendererConfig,
         nodeIntegration: true,
