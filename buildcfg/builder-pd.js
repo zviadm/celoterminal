dd59d11d73c74e44ef4e3909d5ae952f680d692d
Zviad Metreveli <thezviad@gmail.com>
2024-05-11 08:43:13 -0400

    [core] package .app only

--- buildcfg/builder-pd.js
+++ buildcfg/builder-pd.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 const path = require("path")
-const pd = path.join("out", `celoterminal-${process.platform}-${process.arch}`)
+let pd = path.join("out", `celoterminal-${process.platform}-${process.arch}`)
+if (process.platform === "darwin") {
+	pd = path.join(pd, "celoterminal.app")
+}
 console.log(pd)
