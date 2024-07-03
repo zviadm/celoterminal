10180b4158e981b47b02a4491f3518b81b931160
Zviad Metreveli <thezviad@gmail.com>
2024-07-03 12:57:18 -0400

    core: force build OSX binaries for x64 since that is more universal (#351)

--- buildcfg/webpack-path-fix.js
+++ buildcfg/webpack-path-fix.js
@@ -1,11 +1,13 @@
 const path = require("path")
 const fs = require("fs")
 
+const arch = process.argv.length >= 3 ? process.argv[2] : process.arch
+
 if (!fs.existsSync(path.join(".webpack", "main"))) {
 	for (const d of ["renderer", "main"]) {
 		fs.renameSync(
-			path.join(".webpack", process.arch, d),
+			path.join(".webpack", arch, d),
 			path.join(".webpack", d))
 	}
-	fs.rmdirSync(path.join(".webpack", process.arch))
+	fs.rmdirSync(path.join(".webpack", arch))
 }
