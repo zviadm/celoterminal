62152282ba45e2a07ddbdbd6d3f028c3161033b8
Zviad Metreveli <thezviad@gmail.com>
2024-07-03 14:12:01 -0400

    better structure for passing env vars around

--- buildcfg/sslcom-sign.js
+++ buildcfg/sslcom-sign.js
@@ -9,11 +9,22 @@ function sign(configuration) {
 		return true;
 	}
 
-	const scriptPath = process.env.CODE_SIGN_SCRIPT_PATH;
 
 	try {
 		// Execute the sign script synchronously
+		const scriptPath = process.env.CODE_SIGN_SCRIPT_PATH;
+		process.env["INPUT_COMMAND"] = "sign"
 		process.env["INPUT_FILE_PATH"] = configuration.path
+		process.env["INPUT_OVERRIDE"] = "true"
+		process.env["INPUT_MALWARE_BLOCK"] = "false"
+		process.env["INPUT_CLEAN_LOGS"] = "false"
+		process.env["INPUT_JVM_MAX_MEMORY"] = "1024M"
+		process.env["INPUT_ENVIRONMENT_NAME"] = "PROD"
+		process.env["INPUT_USERNAME"] = process.env.CODE_SIGN_SSLCOM_USERNAME
+		process.env["INPUT_PASSWORD"] = process.env.CODE_SIGN_SSLCOM_PASSWORD
+		process.env["INPUT_TOTP_SECRET"] = process.env.CODE_SIGN_SSLCOM_TOTP_SECRET
+		process.env["INPUT_CREDENTIAL_ID"] = process.env.CODE_SIGN_SSLCOM_CREDENTIAL_ID
+
 		const output = execSync(`node "${scriptPath}"`).toString();
 		console.log(`Script output: ${output}`);
 	} catch (error) {
